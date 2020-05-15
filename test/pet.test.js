7281cab79391d708141cf1df5f6ed17d403de25f
Viktoriia Petrova <etrovavica1221@gmail.com>
2020-05-15 19:34:15 +0100

    Add keeping fit

--- test/pet.test.js
+++ test/pet.test.js
@@ -7,9 +7,30 @@ describe('constructor', () => {
 });
 
 describe('growUp', () => {
+  it('increments the age by 1 and the hunger by 5', () => {
+    const pet = new Pet('Fido');
+    pet.growUp();
+    expect(pet.age).toEqual(1);
+    expect(pet.hunger).toEqual(5);
+    expect(pet.fitness).toEqual(7);
+  });
+});
+
+describe('', () => {
   it('increments the age by 1', () => {
     const pet = new Pet('Fido');
     pet.growUp();
     expect(pet.age).toEqual(1);
+  });
+});
+
+describe('walk', () => {
+  it('increases fitness by to a maximum of 10', () => {
+    const pet = new Pet('fido');
+
+    pet.fitness = 8;
+    pet.walk();
+
+    expect(pet.fitness).toEqual(10);
   });
 });
