2b8c77142348650eedf6cb4f69fa2643ed368bf9
Viktoriia Petrova <etrovavica1221@gmail.com>
2020-05-15 23:56:33 +0100

    Add checkup

--- src/pet.js
+++ src/pet.js
@@ -1,11 +1,13 @@
 const MAXIMUM_FITNESS = 10;
 const MINIMUM_HUNGER = 0;
+const ALERT_FITNESS = 3;
+const ALERT_HUNGER = 5;
 
 function Pet(name) {
     this.name = name;
     this.age = 0;
     this.hunger = 0;
-    this.fitness = MAXIMUM_FITNESS;
+    this.fitness = MAXIMUM_FITNESS;    
 }
 
 Pet.prototype.growUp = function() {
@@ -29,5 +31,17 @@ Pet.prototype.feed = function() {
       this.hunger = MINIMUM_HUNGER;
     }
   }
+
+Pet.prototype.checkUp = function() {
+  if (this.fitness <= ALERT_FITNESS && this.hunger >= ALERT_HUNGER) {
+    return 'I am hungry AND I need a walk';
+  } else if (this.fitness <= ALERT_FITNESS) {
+    return 'I need a walk';
+  } else if (this.hunger >= ALERT_HUNGER) {
+    return 'I am hungry';
+  } else {
+    return 'I feel great!';
+  }
+}
 
 module.exports = Pet;
--- test/pet.test.js
+++ test/pet.test.js
@@ -2,13 +2,13 @@ const Pet = require('../src/pet');
 
 describe('constructor', () => {
   it('returns an object', () => {
-    expect(new Pet('Fido')).toBeInstanceOf(Pet);
+    expect(new Pet('fido')).toBeInstanceOf(Pet);
   });
 });
 
 describe('growUp', () => {
   it('increments the age by 1 and the hunger by 5', () => {
-    const pet = new Pet('Fido');
+    const pet = new Pet('fido');
     pet.growUp();
     expect(pet.age).toEqual(1);
     expect(pet.hunger).toEqual(5);
@@ -16,21 +16,11 @@ describe('growUp', () => {
   });
 });
 
-describe('', () => {
-  it('increments the age by 1', () => {
-    const pet = new Pet('Fido');
-    pet.growUp();
-    expect(pet.age).toEqual(1);
-  });
-});
-
 describe('walk', () => {
   it('increases fitness by to a maximum of 10', () => {
     const pet = new Pet('fido');
-
     pet.fitness = 8;
     pet.walk();
-
     expect(pet.fitness).toEqual(10);
   });
 });
@@ -38,11 +28,32 @@ describe('walk', () => {
 describe('feed', () => {
   it('decreases hunger by 3 to a minimum of 0', () => {
     const pet = new Pet('fido');
-
     pet.hunger = 8;
     pet.feed();
-
     expect(pet.hunger).toEqual(5);
   });
 });
+
+describe('checkUp', () => {
+  it("return 'I need a walk' when pet's fitness is 3 or less", () => {
+    const pet = new Pet('fido');
+    pet.fitness = 3;
+    expect(pet.checkUp()).toEqual('I need a walk');
+  });
+  it("return 'I am hungry' when pet's hunger is 5 or more", () => {
+    const pet = new Pet('fido');
+    pet.hunger = 5;
+    expect(pet.checkUp()).toEqual('I am hungry');
+  })
+  it("return 'I am hungry AND I need a walk' when pet's hunger is 5 or more and fitness is 3 or less", () => {
+    const pet = new Pet('fido');
+    pet.hunger = 5;
+    pet.fitness = 3;
+    expect(pet.checkUp()).toEqual('I am hungry AND I need a walk');
+  })
+  it("return 'I feel great!' when neither of the above are true", () => {
+    const pet = new Pet('fido');
+    expect(pet.checkUp()).toEqual('I feel great!');
+  })  
+})
 
