2fe6b9f63deaf9fc4491828c931018233699e5df
Viktoriia Petrova <etrovavica1221@gmail.com>
2020-05-16 14:12:40 +0100

    Add magic numbers

--- test/pet.test.js
+++ test/pet.test.js
@@ -1,5 +1,10 @@
 const Pet = require('../src/pet');
 
+const MAXIMUM_FITNESS = 10;
+const MINIMUM_HUNGER = 0;
+const ALERT_FITNESS = 3;
+const ALERT_HUNGER = 5;
+
 describe('constructor', () => {
   it('returns an object', () => {
     expect(new Pet('fido')).toBeInstanceOf(Pet);
@@ -7,7 +12,7 @@ describe('constructor', () => {
 });
 
 describe('growUp', () => {
-  it('increments the age by 1 and the hunger by 5', () => {
+  it('increments the age by 1 and the hunger by 5 and decrease the fitness property by 3', () => {
     const pet = new Pet('fido');
     pet.growUp();
     expect(pet.age).toEqual(1);
@@ -21,34 +26,34 @@ describe('walk', () => {
     const pet = new Pet('fido');
     pet.fitness = 8;
     pet.walk();
-    expect(pet.fitness).toEqual(10);
+    expect(pet.fitness).toEqual(MAXIMUM_FITNESS);
   });
 });
 
 describe('feed', () => {
   it('decreases hunger by 3 to a minimum of 0', () => {
     const pet = new Pet('fido');
-    pet.hunger = 8;
+    pet.hunger = 2;
     pet.feed();
-    expect(pet.hunger).toEqual(5);
+    expect(pet.hunger).toEqual(MINIMUM_HUNGER);
   });
 });
 
 describe('checkUp', () => {
   it("return 'I need a walk' when pet's fitness is 3 or less", () => {
     const pet = new Pet('fido');
-    pet.fitness = 3;
+    pet.fitness = ALERT_FITNESS;
     expect(pet.checkUp()).toEqual('I need a walk');
   });
   it("return 'I am hungry' when pet's hunger is 5 or more", () => {
     const pet = new Pet('fido');
-    pet.hunger = 5;
+    pet.hunger = ALERT_HUNGER ;
     expect(pet.checkUp()).toEqual('I am hungry');
   })
   it("return 'I am hungry AND I need a walk' when pet's hunger is 5 or more and fitness is 3 or less", () => {
     const pet = new Pet('fido');
-    pet.hunger = 5;
-    pet.fitness = 3;
+    pet.hunger = ALERT_HUNGER ;
+    pet.fitness = ALERT_FITNESS;
     expect(pet.checkUp()).toEqual('I am hungry AND I need a walk');
   })
   it("return 'I feel great!' when neither of the above are true", () => {
